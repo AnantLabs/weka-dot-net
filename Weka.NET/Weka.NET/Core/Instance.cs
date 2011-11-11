59cbe9ff8bd8ea578a261de56b71b618c150702f
antonio.kantek@gmail.com <antonio.kantek@gmail.com@d3ff3377-5091-eba8-0ead-8ab4bca36e0e>
2011-11-11 12:39:07 +0000

    In Weka.NET all objects are immutable

--- Weka.NET/Weka.NET/Core/Instance.cs
+++ Weka.NET/Weka.NET/Core/Instance.cs
@@ -1,12 +1,24 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Weka.NET.Core
 {
     public sealed class Instance
     {
-        public IList<double?> Values { get; set; }
+        public IList<double?> Values { get; private set; }
 
-        public double Weight { get; set; }
+        public double Weight { get; private set; }
+
+        public Instance(IEnumerable<double?> values) : this(weight: 1, values: values)
+        {
+            
+        }
+
+        public Instance(double weight, IEnumerable<double?> values)
+        {
+            Weight = weight;
+            Values = values.ToList().AsReadOnly();
+        }
 
         public double? this[int index]
         {
