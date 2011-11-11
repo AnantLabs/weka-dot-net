091c5a19dfe3789f113a5703a60095b94feb8ba1
antonio.kantek@gmail.com <antonio.kantek@gmail.com@d3ff3377-5091-eba8-0ead-8ab4bca36e0e>
2011-11-11 12:46:42 +0000

    Adding Attribute

--- trunk/Weka.NET/Weka.NET/Core/Attribute.cs
+++ trunk/Weka.NET/Weka.NET/Core/Attribute.cs
@@ -2,16 +2,46 @@
 
 namespace Weka.NET.Core
 {
+    public class Attribute
+    {
+        public string Name { get; private set; }
+
+        public AttributeType Type { get; private set; }
+
+        public int Index { get; private set; }
+
+        public Attribute(string name, AttributeType type, int index)
+        {
+            Name = name;
+            Type = type;
+            Index = index;
+        }
+
+    }
+
     public enum AttributeType
     {
+        NUMERIC,
+        NOMINAL,
         STRING
     }
 
-    public class Attribute
+    public static class AttributeTypeExt
     {
-        public string Name { get; set; }
+        public static bool IsNumeric(this AttributeType type)
+        {
+            return type == AttributeType.NUMERIC;
+        }
 
-        public AttributeType Type { get; set; }
+        public static bool IsNominal(this AttributeType type)
+        {
+            return type == AttributeType.NOMINAL;
+        }
 
+        public static bool IsString(this AttributeType type)
+        {
+            return type == AttributeType.STRING;
+        }
     }
+
 }
