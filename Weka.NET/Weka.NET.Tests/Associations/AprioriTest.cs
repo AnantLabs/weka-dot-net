5f2c644cefadc3a0127ed1648814398c207c2435
antonio.kantek@gmail.com <antonio.kantek@gmail.com@d3ff3377-5091-eba8-0ead-8ab4bca36e0e>
2011-11-13 15:03:28 +0000

    Adding Apriori.BuildSingletons method

--- Weka.NET/Weka.NET.Tests/Associations/AprioriTest.cs
+++ Weka.NET/Weka.NET.Tests/Associations/AprioriTest.cs
@@ -12,17 +12,6 @@ namespace Weka.NET.Tests.Associations
     [TestFixture]
     public class AprioriTest
     {
- 
-        public void CanBuildExtractRulesForNominalDataSet()
-        {
-            var apriori = new Apriori(minSupport:10);
-
-            var dataSet = TestSets.WeatherNominal();
-
-            var rules = apriori.BuildAssociationRules(dataSet);
-
-            //Ensure rules are generated as expected
-        }
 
         [Test]
         public void CanBuildSingletons()
@@ -43,32 +32,44 @@ namespace Weka.NET.Tests.Associations
 
             //Then
             Assert.AreEqual(4, singletons.Count());
-            Assert.IsTrue(singletons.Contains(new ItemSet(counter:0, items:new int?[]{null,0})));
+            Assert.IsTrue(singletons.Contains(new ItemSet(counter: 0, items: new int?[] { null, 0 })));
             Assert.IsTrue(singletons.Contains(new ItemSet(counter: 0, items: new int?[] { null, 1 })));
             Assert.IsTrue(singletons.Contains(new ItemSet(counter: 0, items: new int?[] { 1, null })));
             Assert.IsTrue(singletons.Contains(new ItemSet(counter: 0, items: new int?[] { 1, null })));
         }
 
-        static void Main(string[] args)
+        [Test]
+        public void CanFindLargeItemSet()
         {
+            //Given
             var dataSet = DataSetBuilder.AnyDataSet()
-    .WithNominalAttribute
-        (name: "first_attribute", values: new[] { "first0", "first1" })
-    .WithNominalAttribute
-        (name: "second_attribute", values: new[] { "second0", "second1" })
-    .AddData(values: new[] { "first0", "second0" })
-    .AddData(values: new[] { "first1", "second0" })
-    .AddData(values: new[] { "first0", "second1" })
-        .Build();
+                .WithNominalAttribute
+                    (name: "first_attribute", values: new[] { "first0", "first1" })
+                .WithNominalAttribute
+                    (name: "second_attribute", values: new[] { "second0", "second1" })
+
+                .AddData(values:new[] { "first0", "second0" })
+                .AddData(values:new[] { "first0", "second1" })
+                .AddData(values:new[] { "first0", "second1" })
+                .AddData(values:new[] { "first0", "second1" })
+                .AddData(values:new[] { "first1", "second1" })
+                .AddData(values:new[] { "first1", "second1" })
+
+                .Build();
 
             var apriori = new Apriori(minSupport: 1);
+        }
 
-            var singletons = apriori.BuildAssociationRules(dataSet);
+        public void CanBuildExtractRulesForNominalDataSet()
+        {
+            var apriori = new Apriori(minSupport:10);
 
-            Console.WriteLine(singletons);
-        }    
+            var dataSet = TestSets.WeatherNominal();
 
+            var rules = apriori.BuildAssociationRules(dataSet);
 
+            //Ensure rules are generated as expected
+        }
 
     
     }
---
+++ Weka.NET/Weka.NET.Tests/Associations/ItemSetTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Weka.NET.Associations;
+using Weka.NET.Tests.Core;
+using NUnit.Framework;
+using Weka.NET.Core;
+
+namespace Weka.NET.Tests.Associations
+{
+    [TestFixture]
+    public class ItemSetTest
+    {
+
+
+        [Test]
+        public void ContainedByReturnsTrueIfInstanceContainsItemSet()
+        {
+            var someInstance = new Instance(values: new List<double?> { 1d, 2d, 3d });
+
+            var someItemSet = new ItemSet(counter: 1, items: new List<int?> { null, 2, 3 });
+
+            Assert.IsTrue(someItemSet.ContainedBy(someInstance));
+        }
+
+        [Test]
+        public void ContainedByReturnsFalseIfInstanceDoesntContainItemSet()
+        {
+            var someInstance = new Instance(values: new List<double?> { 1d, 2d });
+
+            var someItemSet = new ItemSet(counter: 1, items: new List<int?> { null, 5 });
+
+            Assert.IsFalse(someItemSet.ContainedBy(someInstance));
+        }
+
+        [Test]
+        public void EnsureConfidenceForRuleDividesConsequenceCounterByPremiseCounter()
+        {
+            var consequence = new ItemSet(counter: 10, items: new List<int?> { 1, 2 });
+
+            var premise = new ItemSet(counter: 10, items: new List<int?> { 1, 2 });
+
+            double actual = ItemSet.ConfidenceForRule(premise, consequence);
+
+            Assert.AreEqual((double)consequence.Counter / (double)premise.Counter, actual, 0d);
+        }
+
+    }
+}
