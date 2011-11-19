baffbc11a1eb45362aec02a980002889872a55e3
antonio.kantek@gmail.com <antonio.kantek@gmail.com@d3ff3377-5091-eba8-0ead-8ab4bca36e0e>
2011-11-19 01:53:00 +0000

    Adding DoubleExt.GreaterOrEqualsTo

---
+++ trunk/Weka.NET/Weka.NET.Tests/Lang/DoubleExtTest.cs
@@ -0,0 +1,29 @@
+﻿using Weka.NET.Lang;
+using NUnit.Framework;
+
+namespace Weka.NET.Tests.Lang
+{
+    [TestFixture]
+    public class DoubleExtTest
+    {
+        [Test]
+        public void GreaterOrEqualsTo_SimpleDiff()
+        {
+            var a = .0004;
+
+            var b = .0002;
+
+            Assert.IsTrue(a.GreaterOrEqualsTo(b));
+        }
+
+        [Test]
+        public void CheckingGreaterOrEqualsTo()
+        {
+            var a = .0002;
+
+            var b = .0004;
+
+            Assert.IsFalse(a.GreaterOrEqualsTo(b));
+        }
+    }
+}
