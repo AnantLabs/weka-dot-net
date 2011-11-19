c43eaccc4d6adf6533e2d3762bd571cc8b29c730
antonio.kantek@gmail.com <antonio.kantek@gmail.com@d3ff3377-5091-eba8-0ead-8ab4bca36e0e>
2011-11-19 01:44:26 +0000

    Adding DoubleExt.GreaterOrEqualsTo

---
+++ Weka.NET/Weka.NET/Lang/DoubleExt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Weka.NET.Lang
+{
+    public static class DoubleExt
+    {
+        readonly public static double SMALL = 1e-6;
+
+        public static bool GreaterOrEqualsTo(this double a, double b)
+        {
+            return (b - a < SMALL);
+        }
+    }
+}
