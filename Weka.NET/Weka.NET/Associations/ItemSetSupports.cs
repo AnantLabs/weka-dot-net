b7996d497bd1d6796698f8cfa832e10f2e23b3b6
antonio.kantek@gmail.com <antonio.kantek@gmail.com@d3ff3377-5091-eba8-0ead-8ab4bca36e0e>
2011-11-25 23:34:47 +0000

    Cleaning code

---
+++ Weka.NET/Weka.NET/Associations/ItemSetSupports.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Weka.NET.Core;
+
+namespace Weka.NET.Associations
+{
+    public class ItemSetSupports
+    {
+        readonly DataSet dataSet;
+
+        readonly IDictionary<ItemSet, int> supports;
+
+        public ItemSetSupports(DataSet dataSet)
+        {
+            this.dataSet = dataSet;
+            this.supports = new Dictionary<ItemSet, int>();
+        }
+
+        public int this[ItemSet index]
+        {
+            get { return supports[index]; }
+        }
+
+        public void AddItemSets(IEnumerable<ItemSet> itemSets)
+        {
+            foreach (var itemSet in itemSets)
+            {
+                AddItemSets(itemSet);
+            }
+        }
+
+        public void AddItemSets(ItemSet itemSet)
+        {
+            if (supports.ContainsKey(itemSet))
+            {
+                return;
+            }
+
+            int count = 0;
+
+            foreach (var instance in dataSet.Instances)
+            {
+                if (itemSet.ContainedBy(instance))
+                {
+                    count++;
+                }
+            }
+
+            supports[itemSet] = count;
+        }
+    }
+}
