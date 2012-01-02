3a160eb33aa23bdc1baa11c720ef9ae1bc39313c
antonio.kantek@gmail.com <antonio.kantek@gmail.com@d3ff3377-5091-eba8-0ead-8ab4bca36e0e>
2012-01-02 01:57:31 +0000

    Adding Filters

---
+++ trunk/Weka.NET/Weka.NET/Filters/DiscretizeFilter.cs
@@ -0,0 +1,37 @@
+﻿namespace Weka.NET.Filters
+{
+    using Weka.NET.Core;
+
+    public class DiscretizeFilter : IFilter
+    {
+        public const int DefaultNumBins = 10;
+
+        public Range<double> DiscretizeRange { get; private set; }
+
+        public int NumBins {get; private set;}
+
+        public double[][] CutPoints { get; private set; }
+
+        public bool UseMDL {get; private set;}
+
+        public bool MakeBinary { get; private set; }
+
+        public bool UseBetterEncoding { get; private set; }
+
+        public bool UseKononenko { get; private set; }
+
+        public bool FindNumBins { get; private set; }
+
+        public bool BatchFinished()
+        {
+            return false;
+        }
+
+
+
+
+    
+    
+    
+    }
+}
